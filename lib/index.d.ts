263cb26d45100469da44e215e8fafc56e1d119ac
Ryan Curtis <rbrcurtis@gmail.com>
2025-09-08 08:35:07 -0500

    less logging

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export { SearchModel } from './core/SearchModel';
+export { SearchService, search, SearchError, VersionConflictError } from './core/SearchService';
+export { DateType, StringType, NumberType, BooleanType, StringArrayType, ObjectType, ObjectArrayType, KeywordType, StringMapType, FieldMetadata, getFieldMetadata, validateFieldType } from './decorators';
+export { id } from './utils/id';
+export { log, logError, logWarn, debug } from './utils/logging';
+export type { SaveEvent, DeleteEvent, SearchOptions } from './core/SearchModel';
---
+++ lib/utils/logging.d.ts
@@ -0,0 +1,4 @@
+export declare function log(message: string, ...args: any[]): void;
+export declare function logError(message: string, ...args: any[]): void;
+export declare function logWarn(message: string, ...args: any[]): void;
+export declare function debug(tags: string | string[], message: string, ...args: any[]): void;
